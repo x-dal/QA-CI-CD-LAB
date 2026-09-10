5b0ba46be1fec94d91ae6393bbbe6260fc26a454
Haider Alsalih <haider@Haiders-MacBook-Pro.local>
2026-09-10 12:07:37 +0200

    Add playwright to CI fix more

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,6 @@
-import {test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-
-test("Home page loads", async ({page}) => {
-    await page.goto("/");
-    await expect(page).toHaveTitle("Create Next App");
+test("home page loads", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle("Create Next App");
 });
