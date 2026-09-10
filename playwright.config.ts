925c717fe958c72c8c3c9dcb60e27040d0aa5e96
Haider Alsalih <haider@Haiders-MacBook-Pro.local>
2026-09-10 15:06:55 +0200

    Add Playwright failure debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,15 +3,20 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   reporter: "html",
+
   use: {
     baseURL: "http://localhost:3000",
-    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
   },
+
   webServer: {
     command: "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
+
   projects: [
     {
       name: "chromium",
