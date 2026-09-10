95a1d9aea0637998f478b5f7615aaa5c4427dfd1
Haider Alsalih <haider@Haiders-MacBook-Pro.local>
2026-09-10 18:07:13 +0200

    Add CI environment variable

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("home page loads", async ({ page }) => {
+  console.log("Running in environment:", process.env.APP_ENV);
   await page.goto("/");
   await expect(page).toHaveTitle("Create Next App");
 });
+
+
 
