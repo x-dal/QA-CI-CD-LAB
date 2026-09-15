ab55ccd91ff96e0ea1a189de690cc00973baf5ec
Haider Alsalih <haider@Haiders-MacBook-Pro.local>
2026-09-15 10:48:48 +0200

    revert-02

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,8 @@ export default function AboutPage() {
 
       <p>This is a simple about page for the QA CI Lab.</p>
       <p>
-        More information can be found on our <a href="/">homepage</a>.
+        More information can be found on our{" "}
+        <a href="https://www.google.com/?zx=1789461931733">homepage</a>.
       </p>
     </main>
   );
